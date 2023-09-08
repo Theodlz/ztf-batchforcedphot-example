cbd4d71ba142e607d0751a4ccda9f3bc5094a80a
Theodlz <theophile.dulaz@gmail.com>
2023-09-08 13:07:56 -0700

    improvements, argparse, and begin support for non-batch

--- retrieve.py
+++ retrieve.py
@@ -9,9 +9,7 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import cpu_count
 
-email: str = "<username>"
-userpass: int = "<password>"
-
+import argparse
 
 ZTF_PHOTOMETRY_CODES = {
     0: "Successful execution",
@@ -32,11 +30,6 @@ def get_lightcurve_url(file):
     return f"https://ztfweb.ipac.caltech.edu{file}"
     
 def fetch_lightcurve(row):
-    if row['exitcode'] in [63, 64, 65, 255]:
-        print(f"Warning: no photometry data found for position (RA, Dec) = ({row['ra']}, {row['dec']}).\n")
-        print(f"Error message: {ZTF_PHOTOMETRY_CODES[row['exitcode']]}\n")
-        return None
-    
     r = requests.get(
         get_lightcurve_url(row['lightcurve']),
         auth=('ztffps', 'dontgocrazy!')
@@ -64,6 +57,8 @@ def fetch_lightcurve(row):
         inplace=True,
     )
     df = df.replace({"null": np.nan})
+    df = df.replace({"" : np.nan})
+
     df['mjd'] = astropy.time.Time(df['jd'], format='jd').mjd
     df['filter'] = df['filter'].str.replace('_', '')
     df['filter'] = df['filter'].str.lower()
@@ -71,10 +66,8 @@ def fetch_lightcurve(row):
     df['mag'] = df['zpdiff'] - 2.5 * np.log10(df['forcediffimflux'])
     df['magerr'] = 1.0857 * df['forcediffimfluxunc'] / df['forcediffimflux']
 
-    snr = df['forcediffimflux'] / df['forcediffimfluxunc'] < 3
-    print(f"Nb of epochs with SNR < 3: {len(df[snr])}\n")
-    print("vs")
-    print(f"Nb of epochs with SNR >= 3: {len(df[~snr])}\n")
+    snr = df['forcediffimflux'] / df['forcediffimfluxunc'] < 3.0
+
     df.loc[snr, 'mag'] = None
     df.loc[snr, 'magerr'] = None
 
@@ -102,25 +95,25 @@ def fetch_lightcurve(row):
     data = df.to_dict(orient='list')
     return data    
 
-def retrieve():
+def retrieve(ra, dec, batch: bool, username: str, password: str, num_proc: int):
     r = None
     n_retry = 0
     df_result = None
     while n_retry < 60:
         base_url = 'https://ztfweb.ipac.caltech.edu/cgi-bin/'
         settings = {
-            'email': email,
-            'userpass': userpass,
+            'email': username,
+            'userpass': password,
             'option': 'All recent jobs',
             'action': 'Query Database'
         }
         r = requests.get(
-            base_url + 'getBatchForcedPhotometryRequests.cgi',
+            base_url + 'getBatchForcedPhotometryRequests.cgi' if batch else base_url + 'getForcedPhotometryRequests.cgi',
             auth=('ztffps', 'dontgocrazy!'),
             params=settings
         )
         if r.status_code == 200:
-            print("Script executed normally and queried the ZTF Batch Forced Photometry database.\n")
+            print(f"Script executed normally and queried the ZTF {'Batch' if batch else ''} Forced Photometry database.\n")
             df_result: pd.DataFrame = pd.read_html(r.text)[0]
             df_result = df_result.replace({np.nan: None})
             with open("List_of_RA_Dec.txt") as f:
@@ -142,23 +135,43 @@ def retrieve():
             else:
                 break
         else:
-            print(f"Error code {r.status_code} returned from the ZTF Batch Forced Photometry database.\n")
+            print(f"Error code {r.status_code} returned from the ZTF {'Batch' if batch else ''} Forced Photometry database.\n")
             print(f"Error message: {r.text}\n")
             print("Retrying in 1 minute...\n")
             time.sleep(60)
             n_retry += 1            
 
     if n_retry == 60:
-        print("Failed to retrieve light curves from the ZTF Batch Forced Photometry database.\n")
-        raise Exception("Failed to retrieve light curves from the ZTF Batch Forced Photometry database.\n")
+        print(f"Failed to retrieve light curves from the ZTF {'Batch' if batch else ''} Forced Photometry database.\n")
+        raise Exception(f"Failed to retrieve light curves from the ZTF {'Batch' if batch else ''} Forced Photometry database.\n")
+    
+    # remove the rows with bad exitcodes[63, 64, 65, 255]
+    good_df_result = df_result[~df_result['exitcode'].isin([63, 64, 65, 255])]
+    good_df_result = good_df_result.replace({np.nan: None})
+    if len(df_result) == 0:
+        print(f"Lightcurves not created successfully for any of the {len(ra)} positions.\n")
+        raise Exception(f"Lightcurves not created successfully for any of the {len(ra)} positions.\n")
+    bad_df_result = df_result[df_result['exitcode'].isin([63, 64, 65, 255])]
+    if len(bad_df_result) > 0:
+        print(f"Lightcurves not created successfully for {len(bad_df_result)} of the {len(ra)} positions.\n")
+        print("The following positions have bad exitcodes:\n")
+        print(bad_df_result[['ra', 'dec', 'exitcode']])
+        print("\n")
+
+    # remove rows with nan values in the lightcurve column
+    good_df_result = good_df_result[~good_df_result['lightcurve'].isna()]
+    if len(good_df_result) == 0:
+        print(f"Lightcurves not created successfully for any of the {len(ra)} positions.\n")
+        raise Exception(f"Lightcurves not created successfully for any of the {len(ra)} positions.\n")
     
     results = {}
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    with ThreadPoolExecutor(max_workers=min(min(num_proc, cpu_count()), len(good_df_result))) as executor:
         futures = []
         for _, row in df_result.iterrows():
             futures.append(executor.submit(fetch_lightcurve, row))
         for future, (_, row) in tqdm(zip(futures, df_result.iterrows()), total=len(df_result), desc='Fetching light curves'):
-            results[f"{row['ra']},{row['dec']}"] = future.result()
+            result= future.result()
+            results[f"({row['ra']}, {row['dec']})"] = result
 
     #DEBUG: save to disk
     with open('results.json', 'w') as fp:
@@ -169,5 +182,55 @@ def retrieve():
 
 
 if __name__ == '__main__':
-    results = retrieve()
-        
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--positions",
+        help="Path to file containing RA Dec positions.",
+        default="List_of_RA_Dec.txt",
+    )
+    parser.add_argument(
+        "--batch",
+        help="Submit all positions in one batch.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--username",
+        help="Username for ZTF forced photometry.",
+        default=None,
+    )
+    parser.add_argument(
+        "--password",
+        help="Password for ZTF forced photometry.",
+        default=None,
+    )
+    parser.add_argument(
+        "--num_proc",
+        help="Number of processes to use when downloading light curves. Defaults to the number of CPUs on the machine.",
+        default=cpu_count(),
+    )
+    args = parser.parse_args()
+
+    if args.username is None:
+        print("Error: username must be provided.")
+        exit(1)
+    if args.password is None:
+        print("Error: password must be provided.")
+        exit(1)
+
+    lines = []
+    try:
+        with open(args.positions) as f:
+            lines = f.readlines()
+        f.close()
+    except:
+        print("Error: could not open file", args.positions)
+        exit(1)
+    
+    print("Number of (ra,dec) pairs =", len(lines))
+    ra, dec = [], []
+    for line in lines:
+        x = line.split()
+        ra.append(float("%.7f"%(float(x[0]))))
+        dec.append(float("%.7f"%(float(x[1]))))
+    
+    results = retrieve(ra, dec, batch=args.batch, username=args.username, password=args.password, num_proc=args.num_proc)
--- submit.py
+++ submit.py
@@ -1,59 +1,140 @@
 import json
 
 import requests
+import argparse
+import urllib.parse
+import astropy.time as atime
 
-email: str = "<username>"
-userpass: int = "<password>"
+def submit_post(ra_list , dec_list, jdstart: float, jdend: float, batch: bool, username: str, password: str):
+    if len(ra_list) > 1 and not batch:
+        print("Error: only one position can be submitted at a time if batch is False.")
+        exit(1)
+    if batch:
+        ra = json.dumps(ra_list)
+        dec = json.dumps(dec_list)
+        jdstart = json.dumps(jdstart) # start JD for all input target positions.
+        jdend = json.dumps(jdend) # end JD for all input target positions.
+    else:
+        ra = ra_list[0]
+        dec = dec_list[0]
 
-def submit_post(ra_list,dec_list):
-    ra = json.dumps(ra_list)
-    dec = json.dumps(dec_list)
-    jds = 2458216.1234 # start JD for all input target positions.
-    jdstart = json.dumps(jds)
-    jde = 2458450.0253 # end JD for all input target positions.
-    jdend = json.dumps(jde)
     payload = {
         "ra": ra,
         "dec": dec,
         "jdstart": jdstart,
         "jdend": jdend,
-        "email": email,
-        "userpass": userpass
+        "email": username,
+        "userpass": password,
     }
     # fixed IP address/URL where requests are submitted:
-    url = "https://ztfweb.ipac.caltech.edu/cgi-bin/batchfp.py/submit"
-    r = requests.post(url,auth=("ztffps", "dontgocrazy!"), data=payload)
-    print("Status_code=",r.status_code)
-    print("Text=",r.text)
+    if batch:
+        url = "https://ztfweb.ipac.caltech.edu/cgi-bin/batchfp.py/submit"
+        r = requests.post(url,auth=("ztffps", "dontgocrazy!"), data=payload)
+    else:
+        params = urllib.parse.urlencode(payload)
+        url = f"https://ztfweb.ipac.caltech.edu/cgi-bin/requestForcedPhotometry.cgi?{params}"
+        r = requests.get(url,auth=("ztffps", "dontgocrazy!"))
+    
+    if r.status_code != 200:
+        print("Error: request failed with status code", r.status_code)
+        exit(1)
+    else:
+        print("Request submitted successfully.")
+        exit(0)
 
 #--------------------------------------------------
 # Main calling program. Ensure "List_of_RA_Dec.txt"
 # contains your RA Dec positions.
-with open("List_of_RA_Dec.txt") as f:
-    lines = f.readlines()
-f.close()
-print("Number of (ra,dec) pairs =", len(lines))
-ralist = []
-declist = []
-i = 0
-for line in lines:
-    x = line.split()
-    radbl = float(x[0])
-    decdbl = float(x[1])
 
-    raval = float("%.7f"%(radbl))
-    decval = float("%.7f"%(decdbl))
+def submit(ra: list, dec: list, jdstart: float, jdend: float, batch: bool, username: str, password: str):
+    if batch:
+        i = 0
+        ralist, declist = [], []
+        for line in lines:
+            x = line.split()
+            radbl = float(x[0])
+            decdbl = float(x[1])
 
-    ralist.append(raval)
-    declist.append(decval)
+            raval = float("%.7f"%(radbl))
+            decval = float("%.7f"%(decdbl))
 
-    i = i + 1
-    rem = i % 1500 # Limit submission to 1500 sky positions.
+            ralist.append(raval)
+            declist.append(decval)
 
-    if rem == 0:
-        submit_post(ralist,declist)
-        ralist = []
-        declist = []
+            i = i + 1
+            rem = i % 1500 # Limit submission to 1500 sky positions.
 
-if len(ralist) > 0:
-    submit_post(ralist,declist)
+            if rem == 0:
+                submit_post(ralist, declist, jdstart, jdend, batch=batch, username=username, password=password)
+                ralist = []
+                declist = []
+
+        if len(ralist) > 0:
+            submit_post(ralist, declist, jdstart, jdend, batch=batch, username=username, password=password)
+    else:
+        for i in range(len(ra)):
+            ralist = [ra[i]]
+            declist = [dec[i]]
+            submit_post(ralist, declist, jdstart, jdend, batch=batch, username=username, password=password)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--positions",
+        help="Path to file containing RA Dec positions.",
+        default="List_of_RA_Dec.txt",
+    )
+    parser.add_argument(
+        "--jdstart",
+        help="Start JD for all input target positions. Defaults to 365 days ago.",
+        default=(atime.Time.now().jd - 365),
+    )
+    parser.add_argument(
+        "--jdend",
+        help="End JD for all input target positions. Defaults to now.",
+        default=atime.Time.now().jd,
+    )
+    parser.add_argument(
+        "--batch",
+        help="Submit all positions in one batch.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--username",
+        help="Username for ZTF forced photometry.",
+        default=None,
+    )
+    parser.add_argument(
+        "--password",
+        help="Password for ZTF forced photometry.",
+        default=None,
+    )
+    args = parser.parse_args()
+
+    if args.username is None:
+        print("Error: username must be provided.")
+        exit(1)
+    if args.password is None:
+        print("Error: password must be provided.")
+        exit(1)
+
+    lines = []
+    try:
+        with open(args.positions) as f:
+            lines = f.readlines()
+        f.close()
+    except:
+        print("Error: could not open file", args.positions)
+        exit(1)
+    
+    print("Number of (ra,dec) pairs =", len(lines))
+    ra, dec = [], []
+    for line in lines:
+        x = line.split()
+        ra.append(float("%.7f"%(float(x[0]))))
+        dec.append(float("%.7f"%(float(x[1]))))
+
+    submit(ra, dec, jdstart=args.jdstart, jdend=args.jdend, batch=args.batch, username=args.username, password=args.password)
+
