d3ee718dadded1c7ac53f0e26a7819ead6c65911
Theodlz <theophile.dulaz@gmail.com>
2023-09-08 09:09:27 -0700

    bugfix

--- retrieve.py
+++ retrieve.py
@@ -28,11 +28,8 @@ ZTF_PHOTOMETRY_CODES = {
     255: "Database connection or query execution error (see log output)",
 }
 
-def get_lightcurve_url(row):
-    if row['lightcurve'] is None:
-        return None
-    else:
-        return f"https://ztfweb.ipac.caltech.edu{row['lightcurve']}"
+def get_lightcurve_url(file):
+    return f"https://ztfweb.ipac.caltech.edu{file}"
     
 def fetch_lightcurve(row):
     if row['exitcode'] in [63, 64, 65, 255]:
@@ -41,7 +38,7 @@ def fetch_lightcurve(row):
         return None
     
     r = requests.get(
-        row['lightcurve'],
+        get_lightcurve_url(row['lightcurve']),
         auth=('ztffps', 'dontgocrazy!')
     )
     if r.status_code != 200:
@@ -70,8 +67,14 @@ def fetch_lightcurve(row):
     df['mjd'] = astropy.time.Time(df['jd'], format='jd').mjd
     df['filter'] = df['filter'].str.replace('_', '')
     df['filter'] = df['filter'].str.lower()
+    
+    df['mag'] = df['zpdiff'] - 2.5 * np.log10(df['forcediffimflux'])
+    df['magerr'] = 1.0857 * df['forcediffimfluxunc'] / df['forcediffimflux']
 
     snr = df['forcediffimflux'] / df['forcediffimfluxunc'] < 3
+    print(f"Nb of epochs with SNR < 3: {len(df[snr])}\n")
+    print("vs")
+    print(f"Nb of epochs with SNR >= 3: {len(df[~snr])}\n")
     df.loc[snr, 'mag'] = None
     df.loc[snr, 'magerr'] = None
 
@@ -102,7 +105,7 @@ def fetch_lightcurve(row):
 def retrieve():
     r = None
     n_retry = 0
-
+    df_result = None
     while n_retry < 60:
         base_url = 'https://ztfweb.ipac.caltech.edu/cgi-bin/'
         settings = {
@@ -118,54 +121,39 @@ def retrieve():
         )
         if r.status_code == 200:
             print("Script executed normally and queried the ZTF Batch Forced Photometry database.\n")
-            break
+            df_result: pd.DataFrame = pd.read_html(r.text)[0]
+            df_result = df_result.replace({np.nan: None})
+            with open("List_of_RA_Dec.txt") as f:
+                lines = f.readlines()
+            f.close()
+            ra, dec = [], []
+            for line in lines:
+                x = line.split()
+                ra.append(float("%.7f"%(float(x[0]))))
+                dec.append(float("%.7f"%(float(x[1]))))
+
+            # only keep the rows that correspond to the RA, Dec positions used for the query
+            df_result = df_result[df_result['ra'].isin(ra) & df_result['dec'].isin(dec)]
+            if len(df_result) < len(ra):
+                print(f"Lightcurves not available yet for {len(ra) - len(df_result)} positions.\n")
+                print("Retrying in 1 minute...\n")
+                time.sleep(60)
+                n_retry += 1
+            else:
+                break
         else:
             print(f"Error code {r.status_code} returned from the ZTF Batch Forced Photometry database.\n")
             print(f"Error message: {r.text}\n")
             print("Retrying in 1 minute...\n")
             time.sleep(60)
-            n_retry += 1
+            n_retry += 1            
 
     if n_retry == 60:
         print("Failed to retrieve light curves from the ZTF Batch Forced Photometry database.\n")
         raise Exception("Failed to retrieve light curves from the ZTF Batch Forced Photometry database.\n")
     
-    # the result (r.text) is an html webpage, which contains a table with the following columns:
-    # 1. reqID
-    # 2. ra
-    # 3. dec
-    # 4. startJD
-    # 5. endJD
-    # 6. created
-    # 7. started
-    # 8. ended
-    # 9. machine
-    # 10. exitcode
-    # 11. lightcurve
-    df_result: pd.DataFrame = pd.read_html(r.text)[0]
-    df_result = df_result.replace({np.nan: None})
-        
-    df_result['lightcurve'] = df_result.apply(get_lightcurve_url, axis=1)
-
-    # read the files that contains the RA, Dec positions used for the query
-    with open("List_of_RA_Dec.txt") as f:
-        lines = f.readlines()
-    f.close()
-    ra, dec = [], []
-    for line in lines:
-        x = line.split()
-        ra.append(float("%.7f"%(float(x[0]))))
-        dec.append(float("%.7f"%(float(x[1]))))
-
-    # only keep the rows that correspond to the RA, Dec positions used for the query
-    df_result = df_result[df_result['ra'].isin(ra) & df_result['dec'].isin(dec)]
-
-    # if there is less rows than RA, Dec pairs, display a warning
-    if len(df_result) < len(ra):
-        print(f"Warning: only {len(df_result)} rows found in the ZTF Batch Forced Photometry database, while {len(ra)} RA, Dec pairs were used for the query.\n")
-
     results = {}
-    with ThreadPoolExecutor(max_workers=4) as executor:
+    with ThreadPoolExecutor(max_workers=1) as executor:
         futures = []
         for _, row in df_result.iterrows():
             futures.append(executor.submit(fetch_lightcurve, row))
